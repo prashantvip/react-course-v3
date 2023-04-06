b037923c24902d44bea849ea04f742f49cedb884
Prashant Kumar <kr.prashantjsr@gmail.com>
2023-04-06 11:52:56 +0530

    useState

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,7 +1,9 @@
+import Starter from './tutorial/01-useState/starter/03-useState-array'
+
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced React</h2>
+      <Starter />
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  // console.log(useState());
+  // console.log(useState(1));
+  // let value = useState(1)[0];
+  // console.log(value);
+  // let func = useState(1)[1];
+  // console.log(func);
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  }
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button type="button" className="btn" onClick={handleClick}>click me</button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
