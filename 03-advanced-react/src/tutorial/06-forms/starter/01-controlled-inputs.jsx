292aeb830b2cc4cd37e84ff86afd311f7fb92628
Prashant Kumar <kr.prashantjsr@gmail.com>
2023-04-25 11:09:34 +0530

    forms

--- 03-advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+
 const ControlledInputs = () => {
-  return <h2>Controlled Inputs</h2>;
+  const [name, setName] = useState('')
+  const [email, setMail] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email)
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>controlled inputs</h4>
+      <div>
+        <label htmlFor="name">name</label>
+        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" value={email} onChange={(e) => setMail(e.target.value)} />
+      </div>
+      <button type="submit">submit</button>
+    </form>
+  );
 };
 export default ControlledInputs;
